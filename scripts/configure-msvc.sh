73d8709743583548267ce5b21d95aed9d28233e3
Olivier Clero <oclero@hotmail.com>
2021-11-09 20:18:43 +0100

    Use vcpkg to add httplib for unit tests

--- scripts/configure-msvc.sh
+++ scripts/configure-msvc.sh
@@ -3,4 +3,6 @@
 declare build_dir="./build"
 declare qt5_dir="C:\Qt\5.15.2\msvc2019_64\lib\cmake\Qt5"
 declare generator="Visual Studio 16 2019"
-cmake -B $build_dir -G "$generator" -DQt5_DIR=$qt5_dir
+declare vcpkg_toolchain_file="C:\vcpkg\scripts\buildsystems\vcpkg.cmake"
+declare vcpkg_triplet="x64-windows"
+cmake -B $build_dir -G "$generator" -DQt5_DIR=$qt5_dir -DCMAKE_TOOLCHAIN_FILE="$vcpkg_toolchain_file" -DVCPKG_TARGET_TRIPLET=$vcpkg_triplet -DVCPKG_APPLOCAL_DEPS=OFF
