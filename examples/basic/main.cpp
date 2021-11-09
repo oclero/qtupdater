5d974e454cc1dafb1bb36b0a38902b9dc10fdb68
Olivier Clero <oclero@hotmail.com>
2021-11-09 20:27:56 +0100

    Add basic flow in example

--- examples/basic/main.cpp
+++ examples/basic/main.cpp
@@ -11,5 +11,40 @@ int main(int argc, char* argv[]) {
 
   oclero::QtUpdater updater;
 
+  QObject::connect(&updater, &oclero::QtUpdater::updateAvailableChanged, &updater, [&updater]() {
+    if (updater.updateAvailable()) {
+      qDebug() << "Update available! You have " << qPrintable(updater.currentVersion()) << " - Latest is "
+               << qPrintable(updater.latestVersion());
+
+      qDebug() << "Downloading changelog...";
+      updater.downloadChangelog();
+    }
+  });
+
+  QObject::connect(&updater, &oclero::QtUpdater::changelogAvailableChanged, &updater, [&updater]() {
+    if (updater.changelogAvailable()) {
+      qDebug() << "Changelog downloaded!\nHere's what's new:";
+      qDebug() << updater.latestChangelog();
+
+      qDebug() << "Downloading installer...";
+      updater.downloadInstaller();
+    }
+  });
+
+  QObject::connect(&updater, &oclero::QtUpdater::changelogAvailableChanged, &updater, [&updater]() {
+    if (updater.installerAvailable()) {
+      qDebug() << "Installer downloaded!";
+
+      qDebug() << "Starting installer...";
+      updater.installUpdate(/* dry */ true);
+    }
+  });
+
+  QObject::connect(&updater, &oclero::QtUpdater::installationFinished, &updater, [&updater]() {
+    qDebug() << "Installation done!";
+  });
+
+  updater.checkForUpdate();
+
   return app.exec();
 }
