d342bbc0c2f9eca5b8234d21d7ab7060b01e0940
Olivier Clero <oclero@hotmail.com>
2022-02-05 10:42:58 +0100

    Add submodule initialization to scripts

--- scripts/configure-msvc.sh
+++ scripts/configure-msvc.sh
@@ -5,4 +5,5 @@ declare qt5_dir="C:/Qt/5.15.2/msvc2019_64/lib/cmake/Qt5"
 declare generator="Visual Studio 16 2019"
 declare vcpkg_toolchain_file="C:/Users/ocler/Documents/Development/vcpkg/scripts/buildsystems/vcpkg.cmake"
 declare vcpkg_triplet="x64-windows"
+git submodule update --init --recursive
 cmake -B $build_dir -G "$generator" -DQt5_DIR=$qt5_dir -DCMAKE_TOOLCHAIN_FILE="$vcpkg_toolchain_file" -DVCPKG_TARGET_TRIPLET=$vcpkg_triplet -DVCPKG_APPLOCAL_DEPS=OFF
