797c2f0df1a0a875138bd5d652c2aca9dd6973d5
Olivier Clero <oclero@hotmail.com>
2022-05-20 18:36:13 +0200

    Add usage example for widget

---
+++ examples/qtwidgets/main.cpp
@@ -0,0 +1,32 @@
+#include <QApplication>
+#include <QDebug>
+#include <QStandardPaths>
+#include <QIcon>
+
+#include <oclero/QtUpdater.hpp>
+#include <oclero/QtUpdateController.hpp>
+
+#include <oclero/QtUpdateWidget.hpp>
+
+int main(int argc, char* argv[]) {
+  Q_INIT_RESOURCE(resources);
+
+  QCoreApplication::setApplicationName("QtWidgetsUpdaterExample");
+  QCoreApplication::setApplicationVersion("1.0.0");
+  QCoreApplication::setOrganizationName("example");
+  QApplication app(argc, argv);
+  QApplication::setWindowIcon(QIcon(":/example/icon.ico"));
+
+  // 1. Create updater backend.
+  oclero::QtUpdater updater;
+  updater.setServerUrl("http://localhost:8000/");
+
+  // 2. Create update dialog controller.
+  oclero::QtUpdateController updateCtrl(updater);
+
+  // 3. Create and show dialog.
+  auto* widget = new oclero::QtUpdateWidget(updateCtrl);
+  widget->show();
+
+  return app.exec();
+}
