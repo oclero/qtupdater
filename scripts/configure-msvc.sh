0b22b4b6d4f68070cef7fed46eb84bcd64f2e7b9
Olivier Clero <oclero@hotmail.com>
2022-05-20 18:00:34 +0200

    Switch configure script to VS2022

--- scripts/configure-msvc.sh
+++ scripts/configure-msvc.sh
@@ -2,7 +2,7 @@
 
 declare build_dir="./build"
 declare qt5_dir="C:/Qt/5.15.2/msvc2019_64/lib/cmake/Qt5"
-declare generator="Visual Studio 16 2019"
+declare generator="Visual Studio 17 2022"
 declare vcpkg_toolchain_file="C:/Users/ocler/Documents/Development/vcpkg/scripts/buildsystems/vcpkg.cmake"
 declare vcpkg_triplet="x64-windows"
 git submodule update --init --recursive
